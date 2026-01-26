c04a9f5ed17f8d9b9b2b1f978a262e9ab29d4710
Vollond <34167687+Vollond@users.noreply.github.com>
2026-01-26 04:57:52 +0200

    Delete init.py

--- init.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
-
-from nodes_latentcutplus import LatentCutPlusExtension
-
-async def comfy_entrypoint():
-    return LatentCutPlusExtension()
