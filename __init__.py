94acf961af29c90f5dc22aa9d9f30fb7e006bd11
Vollond <34167687+Vollond@users.noreply.github.com>
2026-01-26 19:03:34 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
-from .nodes_latentcutplus import LatentCutPlusExtension
+from .nodes_latentcutplus import LatentCutPlusExtension, NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 async def comfy_entrypoint():
     return LatentCutPlusExtension()
+
+# Export old API nodes
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
