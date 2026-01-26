586d8bf77790ed18b2d730d33049323ba174d595
Vollond <34167687+Vollond@users.noreply.github.com>
2026-01-26 04:53:38 +0200

    Adjust sys.path for LatentCutPlusExtension import
    Add system path adjustment for module import.

--- init.py
+++ init.py
@@ -1,4 +1,7 @@
-from .nodes_latentcutplus import LatentCutPlusExtension
+import os, sys
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+
+from nodes_latentcutplus import LatentCutPlusExtension
 
 async def comfy_entrypoint():
     return LatentCutPlusExtension()
